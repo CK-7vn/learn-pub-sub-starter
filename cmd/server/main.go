cd69d43a4778bcb2ace005aef71e10aa53facbb6
CK-7vn <keighan.robichaudgriffin@maine.edu>
2025-01-04 13:16:27 -0500

    ch2 L7

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	connString := "amqp://guest:guest@localhost:5672/"
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		fmt.Println("Failed to connect to rabbit")
+		os.Exit(1)
+	} else {
+		fmt.Println("Connection succesful to rabbit!")
+	}
+	defer conn.Close()
 	fmt.Println("Starting Peril server...")
+	<-signalChan
+	fmt.Println("Shutting down Peril server...")
+
 }
